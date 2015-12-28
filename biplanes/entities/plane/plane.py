456412f0f2941757337e318516b7c0c64205ee8f
Denis Minich <denis.minich.fitr@gmail.com>
2015-12-28 20:06:15 +0300

    Added base phisics

--- biplanes/entities/plane/plane.py
+++ biplanes/entities/plane/plane.py
@@ -1,4 +1,6 @@
 from parabox.behaviour import Movable
+from parabox.phisics import PlainPhisics
+from parabox.structures import ObjectsCollection
 from parabox.visual import ImageView
 
 
@@ -6,3 +8,17 @@ class Plane(Movable, ImageView):
     def __init__(self, *args, **kwargs):
         super(Plane, self).__init__(*args, **kwargs)
         self.add_to_collections(['planes'])
+        self.inner_phisics = ObjectsCollection([
+            PlainPhisics(gravity=(0, 0), affect_objects=[self])],
+            self)
+        self.bind(on_move=self._return_to_scene)
+
+    def _return_to_scene(self, *args, **kwargs):
+        if self.x + self.size[0] > self.parent.size[0]:
+            self.pos[0] = 0
+        if self.x < 0:
+            self.pos[0] = self.parent.size[0] - self.size[0]
+        if self.y + self.size[1] > self.parent.size[1]:
+            self.pos[1] = 0
+        if self.y < 0:
+            self.pos[1] = self.parent.size[1] - self.size[1]
--- biplanes/main.py
+++ biplanes/main.py
@@ -5,7 +5,8 @@ from kivy.lang import Builder
 from kivy.logger import Logger
 from kivy.uix.widget import Widget
 from parabox.base_object import BaseObject
-from parabox.structures import ObjectsCollection
+from parabox.phisics import PlainPhisics
+from parabox.structures import ObjectsCollection, Collector
 
 from biplanes.entities import Plane
 from biplanes.settings import STATIC_PATH
@@ -18,9 +19,23 @@ class Battlefield(BaseObject):
     def __init__(self, *args, **kwargs):
         super(Battlefield, self).__init__(*args, **kwargs)
         self.objects = ObjectsCollection([
-            Plane(size=(35, 35), pos=(500, 300), foreground='red_plane.png'),
-            Plane(size=(35, 35), pos=(300, 300), foreground='blue_plane.png'),],
+            Plane(
+                size=(35, 35),
+                pos=(500, 300),
+                speed_limit=5,
+                foreground='red_plane.png'),
+            Plane(
+                size=(35, 35),
+                pos=(300, 300),
+                speed_limit=5,
+                foreground='blue_plane.png'),],
             self)
+        self.phisics = ObjectsCollection([
+            PlainPhisics(
+                gravity=(1, -1),
+                affect_objects=Collector.get_collection('planes'))],
+            self)
+
 
 class GameApp(App):
    def build(self):
